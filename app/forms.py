1e60c39c33567001849a28e3a10b927eb5033618
Priyanka shravani <sravanikanchi1234@gmail.com>
2021-09-24 20:57:31 +0530

    Initial commit

---
+++ app/forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+ 
+class createuserform(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username','password'] 
+ 
+class addStudentform(ModelForm):
+    class Meta:
+        model=Student
+        fields="__all__"
+ 
